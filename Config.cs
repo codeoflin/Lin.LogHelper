4676fe65800b88b48d5cabb3ae2139e959bd8e65
codeoflin <h@hhh.im>
2020-05-13 13:40:14 +0800

    添加了屏蔽功能

---
+++ Config.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lin.LogHelper
+{
+	/// <summary>
+	/// 配置
+	/// </summary>
+	public static class Config
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public static void LogForDebugDisable()
+		{
+			var trace = new StackTrace();
+			var thisclassname = trace.GetFrame(0).GetMethod().DeclaringType.Name;
+			Type classtype = null;
+			for (int i = 1; trace.GetFrame(i) != null; i++)
+			{
+				classtype = trace.GetFrame(i).GetMethod().DeclaringType;
+				if (classtype.Name != thisclassname) break;
+			}
+			if (!Public_Var.DebugBlackLsit.Contains(classtype.AssemblyQualifiedName)) Public_Var.DebugBlackLsit.Add(classtype.AssemblyQualifiedName);
+		}
+
+        /// <summary>
+		/// 
+		/// </summary>
+		public static void LogForInfomationDisable()
+		{
+			var trace = new StackTrace();
+			var thisclassname = trace.GetFrame(0).GetMethod().DeclaringType.Name;
+			Type classtype = null;
+			for (int i = 1; trace.GetFrame(i) != null; i++)
+			{
+				classtype = trace.GetFrame(i).GetMethod().DeclaringType;
+				if (classtype.Name != thisclassname) break;
+			}
+			if (!Public_Var.InfoBlackLsit.Contains(classtype.AssemblyQualifiedName)) Public_Var.InfoBlackLsit.Add(classtype.AssemblyQualifiedName);
+		}
+        
+        /// <summary>
+		/// 
+		/// </summary>
+		public static void LogForErrorDisable()
+		{
+			var trace = new StackTrace();
+			var thisclassname = trace.GetFrame(0).GetMethod().DeclaringType.Name;
+			Type classtype = null;
+			for (int i = 1; trace.GetFrame(i) != null; i++)
+			{
+				classtype = trace.GetFrame(i).GetMethod().DeclaringType;
+				if (classtype.Name != thisclassname) break;
+			}
+			if (!Public_Var.ErrorBlackLsit.Contains(classtype.AssemblyQualifiedName)) Public_Var.ErrorBlackLsit.Add(classtype.AssemblyQualifiedName);
+		}
+	}//End Class
+}
---
+++ Public_Var.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lin.LogHelper
+{
+	/// <summary>
+	/// 配置
+	/// </summary>
+	internal class Public_Var
+	{
+		/// <summary>
+		/// Debug黑名单
+		/// </summary>
+		/// <returns></returns>
+		internal static HashSet<string> DebugBlackLsit = new HashSet<string>();
+		/// <summary>
+		/// Info黑名单
+		/// </summary>
+		/// <returns></returns>
+		internal static HashSet<string> InfoBlackLsit = new HashSet<string>();
+		/// <summary>
+		/// Error黑名单
+		/// </summary>
+		/// <returns></returns>
+		internal static HashSet<string> ErrorBlackLsit = new HashSet<string>();
+	}
+}
